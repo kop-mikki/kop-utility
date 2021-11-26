c2d567fbad58175830b8733fd49bd33489673dc2
Mikael Máni Jónsson <mikaelmani@kopavogur.is>
2021-11-26 13:50:51 +0000

    added scriptname to log name

--- src/utility/logger.py
+++ src/utility/logger.py
@@ -1,14 +1,18 @@
 import logging
 from datetime import datetime
 
-def create_logger(path):
+def create_logger(path, script_name=""):
     """Creates a logger and logging file it logs into
 
     Args:
         path (Str): Path to the log
     """
     today = datetime.now().strftime("%Y-%b-%d-%H-%M")
-    logname = "{}/{}.log".format(path, today)
+    logname = ""
+    if(script_name is not ""):
+        logname = "{}/{}_{}.log".format(path, script_name, today)
+    else:
+        logname = "{}/{}.log".format(path, today)
     # creating log file and naming the logger
     logging.basicConfig(filename=logname,
                         format="%(asctime)s | %(levelname)s: %(message)s",
